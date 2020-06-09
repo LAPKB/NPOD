a83b82e35419ff45be8e00efcfc02b33d5a6d856
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-06-09 16:59:50 -0700

    Working on Dopt.R

--- R/PSI_2.r
+++ R/PSI_2.r
@@ -1,14 +1,14 @@
 source("prob.r")
 
-PSI_2 <- function(y, t, theta, sigma){
-    N <- length(y[,1])
-    K <- length(theta[1,])
-    psi <- matrix(0,N,K)
+PSI_2 <- function(y, t, theta, sigma) {
+  N <- length(y[, 1])
+  K <- length(theta[1,])
+  psi <- matrix(0, N, K)
 
-    for (i in 1..N) {
-       for (l in 1..K) {
-          psi[i,l] <- prob(y[i,], t, theta[, l], sigma)
-       }
+  for (i in 1:N) {
+    for (l in 1:K) {
+      psi[i, l] <- prob(y[i,], t, theta[, l], sigma)
     }
-    return psi
+  }
+  return(psi)
 }
