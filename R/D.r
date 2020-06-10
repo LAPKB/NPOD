9856f349974922c94fa7377db871a8524a824dfe
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-06-09 17:02:34 -0700

    fix

--- R/D.r
+++ R/D.r
@@ -1,11 +1,11 @@
 source("prob.r")
 
-D<-function(theta_parameter, y, t, sigma, PYL){
-    N <- length(y[,1])
-    D_components = -N
+D <- function(theta_parameter, y, t, sigma, PYL) {
+  N <- length(y[, 1])
+  D_components = -N
 
-    for (i in 1..N) {
-       D_components <- D_components + prob(y[i,], t, theta_parameter, sigma)/PYL(i)
-    }
-    return(D_components)
+  for (i in 1:N) {
+    D_components <- D_components + prob(y[i,], t, theta_parameter, sigma) / PYL(i)
+  }
+  return(D_components)
 }
