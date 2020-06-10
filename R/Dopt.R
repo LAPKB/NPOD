47546f88edbc090bd13c4c12edbb5ac094517d1a
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-06-10 12:39:08 -0700

    translation of Dopt.r done, needs to be tested

--- R/Dopt.R
+++ R/Dopt.R
@@ -16,7 +16,48 @@ Dopt <- function(y, t, theta_0, theta_F, theta_d, sigma) {
     P = PSI_2(y, t, old_theta, sigma)
     ans <- burke(p)
     lam <- ans$lambda
-    ind = (lam > 0.00000001) & (lam > (max(lam) / 1000))
-    ## WORK IN PROGRESS
+    ind <- (lam > 0.00000001) & (lam > (max(lam) / 1000))
+    inb_theta <- old_theta[, ind]
+
+    P = PSI_2(y, t, inb_theta, sigma)
+    ans <- burke(P)
+    F1 <- ans$obj
+    lam <- ans$lambda
+
+    ind2 <- lam > max(lam) / 100
+    new_w <- lam[a] / sum(lam[a])
+    new_theta <- inb_theta[, ind2]
+
+    if (abs(F1 - F0) <= theta_F) {
+      break
+    }
+
+    K <- length(new_theta[1,])
+    pyl <- P * new_w
+    nc_theta <- new_theta
+
+    library(parallel)
+
+    inputs <- 1:K
+    numCores <- detectCores()
+    cl <- makeCluster(numCores)
+    parfor <- function(l) {
+      Dtheta <- function(.theta) { D(.theta, y, t, sigma, pyl) }
+      fun <- function(.theta_parameter) {-1 * Dtheta(.theta_parameter) }
+      cand_theta <- fminsearch(fun, new_theta[, l], options)
+
+      #this is the return value of parfor
+      nc_theta <- prune(new_theta, cand_theta, theta_d)
+    }
+    results <- parLapply(cl, inputs, parfor)
+    #How do we need to process the results???
+
+    old_theta <- nc_theta
+
+    count <- count + 1
+
+
   }
+
+  return(list("count" = count, "theta" = new_theta, "w" = new_w, "LogLikelihood" = F1))
 }
