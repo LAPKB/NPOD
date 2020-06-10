284d2c4db984c45bffbbfe6a48fea32e05063190
Walter Yamada <walter.mas.yamada@gmail.com>
2020-06-10 15:20:23 -0700

    put dimensions of im arguments into burke.R

--- R/burke.R
+++ R/burke.R
@@ -32,28 +32,34 @@ ipm <- function(psi, ldpsi, theta, ldtheta, npoint, nsub, ijob, x, dx, y, dy, fo
 }
 
 burke <- function(PSI) {
-  pyjgx <- matrix(c(0 * seq(1, 10240)), nrow = 10, ncol = 1024)
-  pyjgx[1:10, 1:10] <- as.matrix(PSI) #psi
-  nvar = 5
-  ldpsi <- 10 #nmaxsub
-  theta <- matrix(c(seq(1, 60, 1)), nrow = 10, ncol = nvar + 1) #corden
-  ldtheta <- 1024 #nmaxgrd
-  npoint <- 10 #nactive
-  nsub <- 10
-  ijob <- 0
-  x <- c(seq(1, 10, 1))
-  dx <- rep(0, 10)
-  y <- rep(0, 10)
-  dy <- rep(0, 10)
-  fobj <- 10 ^ -8
-  gap <- 10 ^ -10
-  nvar <- 5
-  keep <- 10
-  ihess <- 0
-  isupres <- 1
-  ipm(pyjgx, ldpsi, theta, ldtheta, npoint, nsub, ijob, x, dx, y, dy, fobj, gap, nvar, keep, ihess, isupres)
-}
+# PSI = pr( observation | support point )
+  nsub <- dim(PSI)[1]
+  npoint <- dim(PSI)[2]
 
+# theta has one support point and it's probability on
+# each row; theta is used only if ijob == 1
+  ldtheta <- 10 # dim(theta)[1]
+  nvar <- 5 # dim(theta)[2] - 1
+  theta <- matrix(c(seq(1,60 , 1)), nrow = 10, ncol = nvar + 1) #corden
+
+# working arrays -- must be npoint long
+  ldpsi = nsub #is passed to low level linear algebra routines
+  x <- c(rep(1/npoint,npoint)) # These are the returned probabilities !!!
+  dx <- c(rep(0,npoint))
+  y <- c(rep(0,npoint))
+  dy <- c(rep(0,npoint))
+
+# will be reset inside of ipm; initial values here are meaningless:
+  fobj <- 10 ^ -8
+  gap <- 10 ^ -10 # will be reset inside of ipm
+  keep <- 0 # will be set > 0 inside of ipm
+
+# flags that control text output or program control
+  ihess <- 0 # flags Hessian error
+  isupres <- 1 # 0 to not suppress error writes, 1 to supress error writes
+  ijob <- 0 # do not condense
+  ipm(psi, ldpsi, theta, ldtheta, npoint, nsub, ijob, x, dx, y, dy, fobj, gap, nvar, keep, ihess, isupres)
+}
 
 
 .test_burke <- function() {
