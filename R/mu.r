1765218a6ada6c688e0efc37e7b5c4503b7cc650
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-06-11 10:42:17 -0700

    moving the individuals around

--- R/mu.r
+++ R/mu.r
@@ -1,5 +1,5 @@
-mu <- function(theta, i) {
-  library(readr)
+mu <- function(theta, i, individuals) {
+
 
   library(ospsuite) # PK-Sim R toolbox
 
@@ -9,15 +9,10 @@ mu <- function(theta, i) {
   y_scaling_factor <- theta[2]
   # Set up the vector as required
   scaling_factors <- c(x_scaling_factor, y_scaling_factor)
-  # par_values = list()
+  par_values = list()
 
-  ### Load the individuals into a list of objects holding their individual characteristics
-  population_data <- read_csv("bupropion baseline demographics - to share - converted to metric units.csv")
 
-  # Convert population_data into IndividualCharacteristics objects used in PK-Sim simulation
-  # Initialize list of individuals
-  number_of_individuals <- length(population_data$ID)
-  par_values <- vector(mode = "list", length = number_of_individuals)
+  #par_values <- vector(mode = "list", length = number_of_individuals)
 
   ### Read in the physiological parameter changes
   # Note that the csv should have the headers [Path,Value,Base_unit]
@@ -36,33 +31,7 @@ mu <- function(theta, i) {
 
 
 
-  individuals <- vector(mode = "list", length = number_of_individuals)
-  for (i in 1:number_of_individuals) {
-    # Process population
-    if (population_data$Population[i] == "European") {
-      their_population <- HumanPopulation$European_ICRP_2002
-    } else if (population_data$Population[i] == "BlackAmerican") {
-      their_population <- HumanPopulation$BlackAmerican_NHANES_1997
-    } else if (population_data$Population[i] == "MexicanAmerican") {
-      their_population <- HumanPopulation$MexicanAmericanWhite_NHANES_1997
-    } else if (population_data$Population[i] == "Asian") {
-      their_population <- HumanPopulation$Asian_Tanaka_1996
-    }
-    # Process gender
-    if (population_data$Sex[i] == "Female") {
-      their_gender <- Gender$Female
-    } else if (population_data$Sex[i] == "Male") {
-      their_gender <- Gender$Male
-    }
-    # Create individual characteristics
-    # Every individual's age will be set to 40 years, the average of the age range given, i.e. 25-55 years
-    individual_chars <- createIndividualCharacteristics(species = Species$Human, population = their_population, gender = their_gender,
-                                                      weight = population_data$`Weight (kg)`[i], weightUnit = "kg",
-                                                      height = population_data$`Height (cm)`[i], heightUnit = "cm",
-                                                      age = 40, ageUnit = "year(s)")
-    # Add to list
-    individuals[[i]] <- individual_chars
-  }
+
 
 
 
--- R/prob.r
+++ R/prob.r
@@ -1,10 +1,11 @@
-prob <- function (y, theta,i, sigma){
-    j <- length(y)
-    z <- matrix(0, 1, 10)
+prob <- function(y, theta, i, sigma, individuals) {
 
-       m <- mu(theta, i)
-       z <- (y-m)^2
-    
-  prob <- (1/(sqrt(2*pi)*sigma))^(j)*exp(-(1/(2*(sigma)^(2)))*sum(z))  
+  j <- length(y)
+  z <- matrix(0, 1, 10)
+
+  m <- mu(theta, i, individuals)
+  z <- (y - m) ^ 2
+
+  prob <- (1 / (sqrt(2 * pi) * sigma)) ^ (j) * exp(-(1 / (2 * (sigma) ^ (2))) * sum(z))
   return(prob)
 }
