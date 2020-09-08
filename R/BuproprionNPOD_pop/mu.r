c74239f47d3cc672148c47ea8a731e5a8964ecd0
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-09-07 22:18:20 -0700

    multi_mu is working but the return its not formatted, it will crash when callin multi_prob. population.csv file created. it seems to be working, needs to be tested.

--- R/BuproprionNPOD_pop/mu.r
+++ R/BuproprionNPOD_pop/mu.r
@@ -61,3 +61,80 @@ mu <- function(theta, t, individual) {
 # y <- mu(theta, t, individuals[[1]])
 # plot(c(0,t), y)
 
+.scale_dissolution_profile <- function(simulation, scaling_factors = c(1, 1)) {
+  dissolution_data_path = "Applications|PO 150 mg - human|SR PO 150 mg - FDA table|Fraction (dose)"
+  if (all(scaling_factors > 0) && (!identical(scaling_factors, c(1, 1)))) {
+    dissolution_data_parameter <- getParameter(dissolution_data_path, simulation)
+    dissolution_data_formula <- dissolution_data_parameter$formula
+    dissolution_data_all_points <- dissolution_data_formula$allPoints
+
+    numPoints <- length(dissolution_data_all_points)
+
+    # Initialize vectors to hold values of table
+    times <- rep(0, numPoints)
+    fractions <- rep(0, numPoints)
+
+    # Read in points
+    for (i in 1:numPoints) {
+      times[i] <- dissolution_data_all_points[[i]]$x
+      fractions[i] <- dissolution_data_all_points[[i]]$y
+    }
+
+    points <- array(c(times, fractions), dim = c(numPoints, 2))
+    # print(points)
+
+    # Apply scaling factor to table
+    new_x <- points[, 1] * scaling_factors[1]
+    new_y <- points[, 2] * scaling_factors[2]
+
+    # print(new_x)
+    # print(new_y)
+
+    dissolution_data_formula$setPoints(new_x, new_y)
+  } else {
+    if (!all(scaling_factors > 0)) {
+      print("Negative scaling factor is not allowed.")
+    }
+    return(NULL)
+  }
+}
+
+
+
+multi_mu <- function(theta, t, individuals) {
+  #Ok, I have a problem, theta[2] and theta[3] are parameters that modify the simulation
+  #so... in order to simulate all the posibilities, it seems that I need to simulate K times
+  #Where K <- length(theta[1,])
+
+
+  sim <- loadSimulation("PO SR 150 mg bupropion to human - Connarn et al 2017 - table - June 2.pkml")
+  population <- loadPopulation("population.csv")
+  pop_size <- length(population$allIndividualIds)
+
+  #Set the times
+  sim$outputSchema$clear() #first clear default
+  sim$outputSchema$addTimePoints(unlist(t)) #add times
+
+  res <- c()
+  t1 <- system.time({
+    for (k in 1:length(theta[1, ])) {
+      #this line sets the ith theta[1] to all the subjects in the population
+      population$setParameterValues("Liver and Intestinal CL|Reference concentration", rep(theta[1, k], pop_size))
+
+      # This line scales the dissolution profile inside the model using the two scaling factors
+      old_diss_prof <- .scale_dissolution_profile(sim, c(theta[2, k], theta[3, k]))
+
+      # Run the simulation
+      res[[k]] <- runSimulation(simulation = sim, population = population)
+
+      # Restore the dissolution profile
+      # TODO: improve this
+      sim <- loadSimulation("PO SR 150 mg bupropion to human - Connarn et al 2017 - table - June 2.pkml")
+      sim$outputSchema$clear() #first clear default
+      sim$outputSchema$addTimePoints(unlist(t)) #add times
+
+    }
+  })
+
+}
+
