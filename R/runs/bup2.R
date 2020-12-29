0c86a6755b41f674922ee10be7186e5d3881d1d0
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-12-29 15:22:57 -0500

    fix bup dataset, from mg/L to umol/L

--- R/runs/bup2.R
+++ R/runs/bup2.R
@@ -2,64 +2,64 @@ rm(list = ls())
 setwd("R")
 source("NPOD.R")
 library(ospsuite) # PK-Sim R toolbox
-   population_file <- "data/bupropion baseline demographics - to share - converted to metric units.csv"
-  pkdata_file <- "data/Bupropion150PKdata.csv"
-  sim_file <- "data/PO SR 150 mg bupropion to human - Connarn et al 2017 - table - scaling factors.pkml"
-  params <- vector(mode = "list", length = 2)
-  params[[1]] <- c(0, 1, 0) # min
-  params[[2]] <- c(10, 1500, 15) #max
-  population_data <- read_csv(population_file)
-  number_of_individuals <- length(population_data$ID)
-  individuals <- vector(mode = "list", length = number_of_individuals)
-  for (i in 1:number_of_individuals) {
-    # Process population
-    if (population_data$Population[i] == "European") {
-      their_population <- HumanPopulation$European_ICRP_2002
-    } else if (population_data$Population[i] == "BlackAmerican") {
-      their_population <- HumanPopulation$BlackAmerican_NHANES_1997
-    } else if (population_data$Population[i] == "MexicanAmerican") {
-      their_population <- HumanPopulation$MexicanAmericanWhite_NHANES_1997
-    } else if (population_data$Population[i] == "Asian") {
-      their_population <- HumanPopulation$Asian_Tanaka_1996
-    }
-    # Process gender
-    if (population_data$Sex[i] == "Female") {
-      their_gender <- Gender$Female
-    } else if (population_data$Sex[i] == "Male") {
-      their_gender <- Gender$Male
-    }
-    individual_chars <- createIndividualCharacteristics(species = Species$Human, population = their_population, gender = their_gender,
-                                                      weight = population_data$`Weight (kg)`[i], weightUnit = "kg",
-                                                      height = population_data$`Height (cm)`[i], heightUnit = "cm",
-                                                      age = 40, ageUnit = "year(s)")
-    individuals[[i]] <- individual_chars
+population_file <- "data/bupropion baseline demographics - to share - converted to metric units.csv"
+pkdata_file <- "data/Bupropion150PKdata_fixed.csv"
+sim_file <- "data/PO SR 150 mg bupropion to human - Connarn et al 2017 - table - scaling factors.pkml"
+params <- vector(mode = "list", length = 2)
+params[[1]] <- c(0, 1, 0) # min
+params[[2]] <- c(10, 20000, 100) #max
+population_data <- read_csv(population_file)
+number_of_individuals <- length(population_data$ID)
+individuals <- vector(mode = "list", length = number_of_individuals)
+for (i in 1:number_of_individuals) {
+  # Process population
+  if (population_data$Population[i] == "European") {
+    their_population <- HumanPopulation$European_ICRP_2002
+  } else if (population_data$Population[i] == "BlackAmerican") {
+    their_population <- HumanPopulation$BlackAmerican_NHANES_1997
+  } else if (population_data$Population[i] == "MexicanAmerican") {
+    their_population <- HumanPopulation$MexicanAmericanWhite_NHANES_1997
+  } else if (population_data$Population[i] == "Asian") {
+    their_population <- HumanPopulation$Asian_Tanaka_1996
   }
+  # Process gender
+  if (population_data$Sex[i] == "Female") {
+    their_gender <- Gender$Female
+  } else if (population_data$Sex[i] == "Male") {
+    their_gender <- Gender$Male
+  }
+  individual_chars <- createIndividualCharacteristics(species = Species$Human, population = their_population, gender = their_gender,
+                                                    weight = population_data$`Weight (kg)`[i], weightUnit = "kg",
+                                                    height = population_data$`Height (cm)`[i], heightUnit = "cm",
+                                                    age = 40, ageUnit = "year(s)")
+  individuals[[i]] <- individual_chars
+}
 
 
-  population_functions <- c(
-      function(.population, .theta, .index = NULL) {
-        pop_theta = c()
-        for (i in 1:length(.theta[1, ])) {
-          pop_theta <- append(pop_theta, rep(.theta[1, i], number_of_individuals))
-        }
-        .population$setParameterValues("Applications|PO 150 mg - human|SR PO 150 mg - FDA table|ScaleFactorX", pop_theta)
-      },
-      function(.population, .theta, .index = NULL) {
-        pop_theta = c()
-        for (i in 1:length(.theta[1, ])) {
-          pop_theta <- append(pop_theta, rep(.theta[2, i], number_of_individuals))
-        }
-        .population$setParameterValues("Applications|PO 150 mg - human|SR PO 150 mg - FDA table|ScaleFactorY", pop_theta)
-      },
-      function(.population, .theta, .index = NULL) {
-        pop_theta = c()
-        for (i in 1:length(.theta[1, ])) {
-          pop_theta <- append(pop_theta, rep(.theta[3, i], number_of_individuals))
-        }
-        .population$setParameterValues("Liver and Intestinal CL|Reference concentration", pop_theta)
+population_functions <- c(
+    function(.population, .theta, .index = NULL) {
+      pop_theta = c()
+      for (i in 1:length(.theta[1, ])) {
+        pop_theta <- append(pop_theta, rep(.theta[1, i], number_of_individuals))
       }
-  )
+      .population$setParameterValues("Applications|PO 150 mg - human|SR PO 150 mg - FDA table|ScaleFactorX", pop_theta)
+    },
+    function(.population, .theta, .index = NULL) {
+      pop_theta = c()
+      for (i in 1:length(.theta[1, ])) {
+        pop_theta <- append(pop_theta, rep(.theta[2, i], number_of_individuals))
+      }
+      .population$setParameterValues("Applications|PO 150 mg - human|SR PO 150 mg - FDA table|ScaleFactorY", pop_theta)
+    },
+    function(.population, .theta, .index = NULL) {
+      pop_theta = c()
+      for (i in 1:length(.theta[1, ])) {
+        pop_theta <- append(pop_theta, rep(.theta[3, i], number_of_individuals))
+      }
+      .population$setParameterValues("Liver and Intestinal CL|Reference concentration", pop_theta)
+    }
+)
 
 
   
-ans <- NPOD(sim_file, pkdata_file, params, individuals, population_functions, size_theta0=2000)
+ans <- NPOD(sim_file, pkdata_file, params, individuals, population_functions, c1=0.01, c2=0.01, size_theta0=500) #c1=0.03, c2=0.05,
