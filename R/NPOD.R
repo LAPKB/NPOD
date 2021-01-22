cbf9d2cb662440f0678ef4b04cc6fd031ed7ccfd
Julian D. Otalvaro <juliandavid347@gmail.com>
2021-01-22 15:09:49 -0500

    Transforming NPOD into a library

--- R/NPOD.R
+++ R/NPOD.R
@@ -1,24 +1,22 @@
 
-library(memoise)
-library(readr)
-library(ggplot2)
-library(tibble)
-library(tidyr)
-library(neldermead)
-library(DiceDesign)
-library(readxl)
+# library(memoise)
+# library(readr)
+# library(ggplot2)
+# library(tibble)
+# library(tidyr)
+# library(neldermead)
+# library(DiceDesign)
+# library(readxl)
 
-source("Dopt.R")
-source("PSI_2.r")
-source("D.r")
-source("burke.R")
-source("prune.r")
-source("mu.r")
-source("prob.r")
+# source("Dopt.R")
+# source("PSI_2.r")
+# source("D.r")
+# source("burke.R")
+# source("prune.r")
+# source("mu.r")
+# source("prob.r")
 
-#this statement not needed on LAPKB machine
-# initPKSim("C:/Users/alona.kryshchenko/Dropbox (CSUCI)/For Alan/SummerGrant/Bupropion with R-Toolbox/PK-Sim 9.0.144")
-
+#'@export
 NPOD <- function(sim_file, pkdata_file, params, individuals, population_functions=c(), simulation_functions=c(), noise=F, model=NULL, c0=0.5, c1=0.1, c2=0, theta_0=NULL,size_theta0=NULL) {
 
   #TODO: WIP global variables.
@@ -289,6 +287,45 @@ posterior <- function(res){
 
 }
 
+posterior_observations <- function(ans, posterior){
+  pkdata <- read.csv(pkdata_file)
+  number_of_individuals <- ncol(pkdata) - 1
+  time <- vector(mode = "list", length = number_of_individuals)
+  y_old <- vector(mode = "list", length = number_of_individuals)
+
+
+  for (i in 1:number_of_individuals) {
+    time[[i]] <- pkdata[, 1][pkdata[, i + 1] != 999] * 1.0 #* 60
+    y_old[[i]] <- pkdata[, i + 1][pkdata[, i + 1] != 999]
+  }
+  ind <- c(0)
+  for (i in 1:number_of_individuals) { ind[i] = length(y_old[[i]]) != 0 }
+  t <- time[as.logical(ind)]
+  y <- y_old[as.logical(ind)]
+
+  simulator<<-"PKSIM"
+  sim_file<<-sim_file
+  simulation_functions<<-c()
+  population_functions<<-population_functions
+  t1 <- system.time({
+  m <- cached_mu(ans$theta, t)
+  })
+
+  plot(c(0, max(unlist(y))), c(0, max(unlist(m))), type = "l", col = "black", xlab = "Observed", ylab = "posterior")
+  total_wavg = c()
+  for (l in 1:length(y)) {
+    wavg = rep(0, length(nrow(m))) 
+    for (sup in 1:length(ncol(m))) {
+      wavg = wavg + m[[l, sup]] * posterior[l, sup]
+    }
+    points(y[[l]], wavg, pch = 15)
+    total_wavg[[l]] = wavg
+  }
+  data <- data.frame(x = unlist(y), y = unlist(total_wavg))
+  line <- lm(formula = y ~ x, data = data)
+
+}
+
 correlation <- function(res, individuals, params = c('Age', 'Weight', 'Height')){
   #TODO: make this general
   #P(x/Y) = post
@@ -389,6 +426,7 @@ spaghetti_plot_w <-function(){
 spaghetti_plot <-function(){
   library(ggplot2)
   library(purrr)
+  library(ggdark)
   # plot(c(0, max(unlist(t))), c(0, pmax(max(unlist(y)), max(unlist(m)) )), col = "white", xlab = "Time (m)", ylab = "Concentration")
   # #plot(c(0, 1.5), c(0, 1.5), type = "l", col = "black", xlab = "Observed", ylab = "Predicted")
   # total_wavg = c()
@@ -409,13 +447,16 @@ spaghetti_plot <-function(){
   normw <- (w-minw)/(maxw-minw)
   reduce(1:length(y), function(x,i){ 
     reduce(1:length(w), function(y, j){
-        y + geom_line(data = data.frame(t=t[[i]], y=m[[i, j]], normw=normw[j]), aes(x=t, y=y, color=normw), size = 0.6, alpha = normw[j])
+        y + geom_line(data = data.frame(t=t[[i]], y=m[[i, j]], normw=normw[j]), aes(x=t, y=y), alpha = normw[j])#, color=normw), size = 0.6, alpha = normw[j])
     },
-    .init=x + geom_line(data = data.frame(t=t[[i]], y=y[[i]]), aes(x=t, y=y))
+    .init=x + geom_line(data = data.frame(t=t[[i]], y=y[[i]]), aes(x=t, y=y, color="red"), size=1)
     )  
   },
   .init=ggplot()
-  ) + scale_color_gradientn(colours = rainbow(length(w))) #+ scale_color_gradient(low="blue", high="red")
+  ) + dark_theme_linedraw() +
+  theme(panel.grid.major = element_line(color = "grey50")) +
+  theme(panel.grid.minor = element_line(color = "grey50"))#+ scale_color_gradient(low="blue", high="red")
+   #+ scale_color_gradientn(colours = rainbow(length(w))) +
   
 
   # data <- data.frame(x = unlist(y), y = unlist(total_wavg))
@@ -426,35 +467,47 @@ marginals_plot <- function(ans){
   library(ggplot2)
 
   # # Create data
-  data1 <- data.frame(x = ans$theta[1,], y = ans$w)
-  data2 <- data.frame(x = ans$theta[2,], y = ans$w)
-  data3 <- data.frame(x = ans$theta[3,], y = ans$w)
+  # data1 <- data.frame(theta = original, y = rep(1/length(original), length(original)))
+  # data2 <- data.frame(theta= sol, y = w)
+  data2 <- data.frame(theta= twins_sol, y = twins_w)
+  # data3 <- data.frame(x = ans$theta[3,], y = ans$w)
+  # initials <- data.frame(x=c(1), y=c(0.2))
   # # data2 <- data.frame(x = c(1.571292659, 1.684000469, 1.897188772, 1.334296808, 3.507619028, 1.965186011, 1.654506763, 2.959986382, 1.651944217, 1.684047026, 1.994727369, 1.749076709, 1.944705938, 2.022695536, 1.878534566, 0.122129378, 0.104367722, 0.134260968, 0.063060173, 0.066073962, 0.116931539, 0.104979233, 0.116084543, 0.073854101, 0.097529239, 0.083336113, 0.116909087, 0.147036329, 0.126209817, 0.136675505), y = rep(1/30,30))
 
 
   # # # Plot
-  p1 <- ggplot(data1, aes(x = x, y = y)) +
-  geom_point() +
-  geom_segment(aes(x = x, xend = x, y = 0, yend = y)) +
-  ggtitle("Marginals - theta[1] (Scalefx)") +
+  p1 <- ggplot(data1,aes(x=theta,y=y)) +
+  geom_col(alpha = 0.3, width=0.0001, color="red", fill="red") +
+  geom_col(data=data2, alpha = 0.3, width=0.0001, color="lightblue", fill="lightblue") +
+  dark_theme_linedraw() +
+  geom_vline(xintercept=c(weighted.mean(original, rep(1/length(original), length(original))),weighted.mean(sol, w)),col=c("red","lightblue"), linetype=2)+
+  theme(panel.grid= element_line(color = "grey30")) +
+  ggtitle("Marginals - Liver Enzyme|Reference concentration") +
   xlab("Liver Enzyme|Reference concentration") +
-  ylab("Weight")
+  ylab("Weight") + coord_trans(x="log10")
 
-  p2 <- ggplot(data2, aes(x = x, y = y)) +
-  geom_point() +
-  geom_segment(aes(x = x, xend = x, y = 0, yend = y)) +
+  p2 <- ggplot(data2,aes(x=x,y=y)) +
+
+  geom_col(alpha = 0.5, width=0.1) +
+  dark_theme_linedraw() +
+  geom_vline(xintercept=c(1,weighted.mean(ans$theta[2, ], ans$w)),col=c("red","blue"))+
+  theme(panel.grid= element_line(color = "grey30")) +
   ggtitle("Marginals - theta[2] (Scalefy)") +
-  xlab("Liver Enzyme|Reference concentration") +
+  xlab("theta[2] (Scalefy)") +
   ylab("Weight")
 
-  p3 <- ggplot(data3, aes(x = x, y = y)) +
-  geom_point() +
-  geom_segment(aes(x = x, xend = x, y = 0, yend = y)) +
-  ggtitle("Marginals - theta[3] (Liver intestinal CL ref)") +
-  xlab("Liver Enzyme|Reference concentration") +
+  p3 <- ggplot(data3,aes(x=x,y=y)) +
+
+  geom_col(alpha = 0.5, width=0.1) +
+  dark_theme_linedraw() +
+  geom_vline(xintercept=c(1,weighted.mean(ans$theta[3, ], ans$w)),col=c("red","blue"))+
+  theme(panel.grid= element_line(color = "grey30")) +
+  ggtitle("Marginals - theta[3] (theta[3] (Liver Enzyme|Reference concentration))") +
+  xlab("theta[1] (theta[3] (Liver Enzyme|Reference concentration))") +
   ylab("Weight")
 
-  gridExtra::grid.arrange(p1, p2, p3, ncol = 3)
+
+  gridExtra::grid.arrange(p1, p2, p3, ncol = 3) 
 }
 
 # P <- PSI_2(y, t, theta, sigma)
