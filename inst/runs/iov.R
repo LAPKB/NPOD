4670a3578249e4dffcbaee066cae2c7826823fe6
Julian D. Otalvaro <juliandavid347@gmail.com>
2022-08-16 15:34:04 -0500

    NPOD handling multiple occasions

--- inst/runs/iov.R
+++ inst/runs/iov.R
@@ -10,4 +10,40 @@ params[[1]] <- c(0.1) # min
 params[[2]] <- c(100) # max
 raw_data <- parse_data_file(data_file)
 demographics <- raw_data %>% get_demographics()
-concentrations <- raw_data %>% get_concentrations()
+number_of_individuals <- 100
+number_of_occassions <- raw_data$occasion %>% max()
+
+individuals <- vector(mode = "list", length = number_of_individuals)
+
+for (i in 1:number_of_individuals) {
+  if (demographics$gender[i] == "Female") {
+    their_gender <- Gender$Female
+  } else if (demographics$gender[i] == "Male") {
+    their_gender <- Gender$Male
+  }
+  individual_chars <- createIndividualCharacteristics(
+    species = Species$Human, population = HumanPopulation$European_ICRP_2002,
+    gender = their_gender,
+    weight = demographics$"organism|weight"[i], weightUnit = "kg",
+    height = demographics$"organism|height"[i], heightUnit = "dm",
+    age = demographics$"organism|age"[i], ageUnit = "year(s)"
+  )
+  individuals[[i]] <- individual_chars
+}
+
+
+parameter_paths <- c("Liver Enzyme|Reference concentration")
+population_functions <- get_population_functions(parameter_paths, number_of_individuals)
+
+occasions <- vector(mode = "list", length = number_of_individuals)
+
+
+for (i in 1:number_of_occassions) {
+  pkdata_file <- paste0("conc_", i, ".csv")
+  write.csv(raw_data %>% get_concentrations(occ = i), pkdata_file, row.names = FALSE)
+  readLines(pkdata_file) %>%
+    gsub("\"", "", .) %>%
+    gsub("time", "Time", .) %>%
+    writeLines(pkdata_file)
+  occasions[[i]] <- NPOD(sim_file, pkdata_file, params, individuals, population_functions, c0 = 0.05, c1 = 0.01)
+}
